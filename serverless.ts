85c50c572eb6e73210ba2b550bef00aabae7ce58
ArpitaDeb <arpitadeb1990@gmail.com>
2021-02-19 16:59:42 -0500

    aws typescript serverless api

---
+++ serverless.ts
@@ -0,0 +1,42 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  service: 'aws-typescapi',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    profile: 'serverlessUser',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: { 
+    getCondition: {
+      handler: 'lambdas/getCondition.handler',
+      events: [
+          {
+              http: {
+                  path: 'get-illness',
+                  method: 'get',
+                  cors: true,
+              },
+          },
+      ],
+  },
+  },
+}
+
+module.exports = serverlessConfiguration;
